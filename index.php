5b6b76c2f45d5d9d2ed6ecc2e45acc7d1c2f9888
Nguyen Hoang Kha <bkit9x@gmail.com>
2018-05-10 11:27:13 +0700

    upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+<body style="background-color:#000;color:#0f0;">
+    <form method="post" action="" enctype="multipart/form-data">
+        <input type="file" name="avatar"/>
+        <input type="submit" name="uploadclick" value="Upload"/>
+    </form>
+    <font color="#F00">
+    <?php // Xử Lý Upload
+  
+    // Nếu người dùng click Upload
+    if (isset($_POST['uploadclick']))
+    {
+        // Nếu người dùng có chọn file để upload
+        if (isset($_FILES['avatar']))
+        {
+            // Nếu file upload không bị lỗi,
+            // Tức là thuộc tính error > 0
+            if ($_FILES['avatar']['error'] > 0)
+            {
+                echo 'File Upload Bị Lỗi';
+            }
+            else{
+                // Upload file
+                move_uploaded_file($_FILES['avatar']['tmp_name'], './file/'.$_FILES['avatar']['name']);
+
+                file_put_contents("data",'
+/f/'.$_FILES['avatar']['name'] ,FILE_APPEND);
+                echo 'File Uploaded';
+            }
+        }
+        else{
+            echo 'Bạn chưa chọn file upload';
+        }
+    }
+echo "</font>";
+echo html_entity_decode(str_replace('
+','<br/>',file_get_contents("data")));
+
+?>
+</body>
+</html>
